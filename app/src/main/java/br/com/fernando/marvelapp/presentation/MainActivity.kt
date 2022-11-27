e4646d6c242ece69f474ed00f34e3a9c29786f18
Fernando <fernandocsrmartins@gmail.com>
2022-11-27 12:02:15 -0300

    Configuração do módulo core do projeto

--- app/src/main/java/br/com/fernando/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/br/com/fernando/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.fernando.marvelapp
+package br.com.fernando.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fernando.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
